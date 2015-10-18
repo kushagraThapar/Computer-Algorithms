b017f3007518fe3b8106dc8118f145f1716406cc
Kushagra Thapar <kushuthapar@gmail.com>
2015-10-18 14:04:06 -0500

    packaging changes

--- Sorting-Algorithms/src/com/searching/binarysearch/BinarySearchClass.java
+++ Sorting-Algorithms/src/com/searching/binarysearch/BinarySearchClass.java
@@ -7,13 +7,24 @@ import com.sorting.merge.MergeSortClass;
  */
 public class BinarySearchClass {
 
+    private static int count = 0;
+
     public static void main(String[] args) {
         int array[] = new int[]{0,-1,2,3,4,10,-5,-6,-7,-8};
         int resultArray[] = MergeSortClass.mergeSort(array, 0, array.length - 1);
-        System.out.println(binarySearch(resultArray, 0, resultArray.length - 1, 0));
+        int abs[] = null;
+        int abc[] = new int[0];
+        if(resultArray != null && resultArray.length > 0) {
+            System.out.println(binarySearch(resultArray, 0, resultArray.length - 1, 11));
+        }
+        System.out.printf("Binary Search called : [" + count + "] times");
     }
 
     public static boolean binarySearch(int[] elements, int start, int end, int searchElement) {
+        count++;
+        if(elements.length == 1) {
+            return (searchElement == elements[0]);
+        }
         if(start > end) {
             return false;
         }
